2a8dbd27a71276fb23a9aeccac4907b5126f6626
Mate Lakat <mate.lakat@citrix.com>
2012-11-01 14:59:36 +0000

    configure coverage

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 virtualenv --no-site-packages .env
 . .env/bin/activate
 pip install -r harness-requires
-nosetests -v -x --cover-html
+nosetests -v -x --cover-html --cover-erase --with-coverage --cover-package=xenapi_nfs_driver
