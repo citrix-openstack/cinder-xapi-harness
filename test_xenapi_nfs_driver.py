3fca5a7eb016488e927a0fbb24934c27d3095665
Mate Lakat <mate.lakat@citrix.com>
2012-11-01 14:19:37 +0000

    refactor: proper connection and session classes

--- test_xenapi_nfs_driver.py
+++ test_xenapi_nfs_driver.py
@@ -1,115 +1,113 @@
-import contextlib
-import XenAPI
 import unittest
 import xenapi_nfs_driver
 import os
 import params
-import XenAPI
 
 
-class Session(object):
-    'Similar to the one we have in openstack'
-    def __init__(self):
-        self.url = params.xapi_url
-        self.user = params.xapi_user
-        self.password = params.xapi_pass
+class TestSessionFactory(unittest.TestCase):
+    def test_exception_raised_if_session_is_used_after_closed(self):
+        sessionFactory = xenapi_nfs_driver.SessionFactory(
+            params.xapi_url,
+            params.xapi_user,
+            params.xapi_pass
+        )
+        session = sessionFactory.get_session()
 
-        self.XenAPI = XenAPI
-        self.session = self._create_sess()
-        self.host_uuid = self._get_host_uuid()
+        session.get_srs()
 
-    def _create_sess(self):
-        session = XenAPI.Session(self.url)
-        session.login_with_password(self.user, self.password)
-        return session
+        session.close()
 
-    @contextlib.contextmanager
-    def _get_session(self):
-        yield self.session
-
-    def _get_host_uuid(self):
-        with self._get_session() as session:
-            host_ref = session.xenapi.session.get_this_host(session.handle)
-            return session.xenapi.host.get_uuid(host_ref)
-
-    def call_xenapi(self, method, *args):
-        with self._get_session() as session:
-            return session.xenapi_request(method, args)
-
-    def get_xenapi_host(self):
-        with self._get_session() as session:
-            return session.xenapi.host.get_by_uuid(self.host_uuid)
+        with self.assertRaises(xenapi_nfs_driver.XenAPIException):
+            session.get_srs()
 
 
-class TestVolumeCreation(unittest.TestCase):
+class XenAPISessionBased(unittest.TestCase):
     def setUp(self):
-        self.driver = xenapi_nfs_driver.XenAPINFSDriver(Session())
+        self.sessionFactory = xenapi_nfs_driver.SessionFactory(
+            params.xapi_url,
+            params.xapi_user,
+            params.xapi_pass
+        )
+        self.session = self.sessionFactory.get_session()
         self.disconnect_all_nfs_srs()
-        self.host_ref = self.driver.session.get_xenapi_host()
-        self.host_uuid = self.driver.session.host_uuid
+        self.host_ref = self.session.get_this_host()
+        self.host_uuid = self.session.get_host_uuid(self.host_ref)
+        self.assertFalse(self.session.get_pool())
+
+    def tearDown(self):
+        self.session.close()
 
     def disconnect_all_nfs_srs(self):
-        for sr_ref in self.driver.get_srs():
-            if self.driver.is_nfs_sr(sr_ref):
-                self.driver.unplug_pbds_and_forget_sr(sr_ref)
+        for sr_ref in self.session.get_srs():
+            if self.session.is_nfs_sr(sr_ref):
+                self.session.unplug_pbds_and_forget_sr(sr_ref)
 
+
+
+class XenAPISessionTest(XenAPISessionBased):
     def test_new_sr_on_nfs_connects_an_sr_and_disconnects(self):
-        driver = self.driver
-        number_of_srs_before = len(driver.get_srs())
+        session = self.session
+        number_of_srs_before = len(session.get_srs())
 
-        with driver.new_sr_on_nfs(self.host_ref, params.nfs_server,
+        with session.new_sr_on_nfs(self.host_ref, params.nfs_server,
                                   params.nfs_serverpath):
             self.assertEquals(
                 number_of_srs_before + 1,
-                len(driver.get_srs()))
+                len(session.get_srs()))
 
         self.assertEquals(
             number_of_srs_before,
-            len(driver.get_srs()))
+            len(session.get_srs()))
 
     def filenames_on_export(self):
         return set(os.listdir(params.exported_catalog))
 
     def test_sr_directory_is_not_removed(self):
-        driver = self.driver
+        session = self.session
 
         filenames_before = self.filenames_on_export()
 
-        with driver.new_sr_on_nfs(self.host_ref, params.nfs_server,
+        with session.new_sr_on_nfs(self.host_ref, params.nfs_server,
                                   params.nfs_serverpath) as sr_ref:
-            uuid = driver.get_sr_uuid(sr_ref)
+            uuid = session.get_sr_uuid(sr_ref)
 
         filenames_after = self.filenames_on_export()
 
         self.assertEquals([uuid], list(filenames_after - filenames_before))
 
     def test_vdi_create_does_not_introduce_vdi(self):
-        driver = self.driver
+        session = self.session
 
-        vdi_list = driver.get_vdis()
+        vdi_list = session.get_vdis()
 
-        with driver.new_sr_on_nfs(self.host_ref, params.nfs_server,
+        with session.new_sr_on_nfs(self.host_ref, params.nfs_server,
                                   params.nfs_serverpath) as sr_ref:
-            vdi_ref = driver.create_new_vdi(sr_ref, 1)
+            vdi_ref = session.create_new_vdi(sr_ref, 1)
             self.assertEquals(
-                set(vdi_list + [vdi_ref]), set(driver.get_vdis()))
+                set(vdi_list + [vdi_ref]), set(session.get_vdis()))
 
-        self.assertEquals(vdi_list, driver.get_vdis())
+        self.assertEquals(vdi_list, session.get_vdis())
 
     def test_re_attach_an_sr(self):
-        driver = self.driver
+        session = self.session
 
-        with driver.new_sr_on_nfs(self.host_ref, params.nfs_server,
+        with session.new_sr_on_nfs(self.host_ref, params.nfs_server,
                                   params.nfs_serverpath) as sr_ref:
-            sr_uuid = driver.get_sr_uuid(sr_ref)
-            sr_count = len(driver.get_srs())
-            pbd_count = len(driver.get_pbds())
+            sr_uuid = session.get_sr_uuid(sr_ref)
+            sr_count = len(session.get_srs())
+            pbd_count = len(session.get_pbds())
 
-        driver.plug_nfs_sr(
+        session.plug_nfs_sr(
             self.host_ref, params.nfs_server, params.nfs_serverpath, sr_uuid)
 
-        self.assertEquals(sr_count, len(driver.get_srs()))
-        self.assertEquals(pbd_count, len(driver.get_pbds()))
+        self.assertEquals(sr_count, len(session.get_srs()))
+        self.assertEquals(pbd_count, len(session.get_pbds()))
+
+
+class XenAPINFSDriverTest(XenAPISessionBased):
+    def setUp(self):
+        super(XenAPINFSDriverTest, self).setUp()
+        self.driver = xenapi_nfs_driver.XenAPINFSDriver(self.sessionFactory)
 
     def test_create_a_new_nfs_backed_volume_returns_sr_uuid(self):
         driver = self.driver
@@ -139,8 +137,8 @@ class TestVolumeCreation(unittest.TestCase):
     def test_re_attach_an_nfs_backed_volume_increases_number_of_vdis_srs(self):
         driver = self.driver
 
-        original_number_of_srs = len(driver.get_srs())
-        original_number_of_vdis = len(driver.get_vdis())
+        original_number_of_srs = len(self.session.get_srs())
+        original_number_of_vdis = len(self.session.get_vdis())
 
         connection_data = driver.create_volume(
             self.host_uuid, params.nfs_server, params.nfs_serverpath, 1)
@@ -149,17 +147,17 @@ class TestVolumeCreation(unittest.TestCase):
 
         self.assertEquals(
             original_number_of_srs + 1,
-            len(driver.get_srs()))
+            len(self.session.get_srs()))
 
         self.assertEquals(
             original_number_of_vdis + 1,
-            len(driver.get_vdis()))
+            len(self.session.get_vdis()))
 
     def test_disconnect_a_volume(self):
         driver = self.driver
 
-        original_number_of_srs = len(driver.get_srs())
-        original_number_of_vdis = len(driver.get_vdis())
+        original_number_of_srs = len(self.session.get_srs())
+        original_number_of_vdis = len(self.session.get_vdis())
 
         connection_data = driver.create_volume(
             self.host_uuid, params.nfs_server, params.nfs_serverpath, 1)
@@ -170,8 +168,8 @@ class TestVolumeCreation(unittest.TestCase):
 
         self.assertEquals(
             original_number_of_srs,
-            len(driver.get_srs()))
+            len(self.session.get_srs()))
 
         self.assertEquals(
             original_number_of_vdis,
-            len(driver.get_vdis()))
+            len(self.session.get_vdis()))
--- xenapi_nfs_driver.py
+++ xenapi_nfs_driver.py
@@ -1,44 +1,70 @@
 import contextlib
 
 
-class XenAPINFSDriver(object):
-    def __init__(self, session):
-        self.session = session
+class XenAPIException(Exception):
+    def __init__(self, original_exception):
+        super(XenAPIException, self).__init__(str(original_exception))
+        self.original_exception = original_exception
+
+
+class XenAPISession(object):
+    def __init__(self, session, exception_to_convert):
+        self._session = session
+        self.exception_to_convert = exception_to_convert
+        self.handle = self._session.handle
+
+    def close(self):
+        return self.call_xenapi('logout')
+
+    def call_xenapi(self, method, *args):
+        try:
+            return self._session.xenapi_request(method, args)
+        except self.exception_to_convert as e:
+            raise XenAPIException(e)
+
+    def get_pool(self):
+        return self.call_xenapi('session.get_pool', self.handle)
+
+    def get_this_host(self):
+        return self.call_xenapi('session.get_this_host', self.handle)
 
     def get_pbds(self):
-        return self.session.call_xenapi('PBD.get_all')
+        return self.call_xenapi('PBD.get_all')
 
     def get_srs(self):
-        return self.session.call_xenapi('SR.get_all')
+        return self.call_xenapi('SR.get_all')
 
     def get_vdis(self):
-        return self.session.call_xenapi('VDI.get_all')
+        return self.call_xenapi('VDI.get_all')
 
     def get_sr_record(self, sr_ref):
-        return self.session.call_xenapi('SR.get_record', sr_ref)
+        return self.call_xenapi('SR.get_record', sr_ref)
 
     def get_vdi_record(self, vdi_ref):
-        return self.session.call_xenapi('VDI.get_record', vdi_ref)
+        return self.call_xenapi('VDI.get_record', vdi_ref)
+
+    def get_host_record(self, host_ref):
+        return self.call_xenapi('host.get_record', host_ref)
 
     def forget_sr(self, sr_ref):
-        self.session.call_xenapi('SR.forget', sr_ref)
+        self.call_xenapi('SR.forget', sr_ref)
 
     def unplug_pbd(self, pbd_ref):
-        self.session.call_xenapi('PBD.unplug', pbd_ref)
+        self.call_xenapi('PBD.unplug', pbd_ref)
 
     def scan_sr(self, sr_ref):
-        self.session.call_xenapi('SR.scan', sr_ref)
+        self.call_xenapi('SR.scan', sr_ref)
 
     def get_vdi_by_uuid(self, vdi_uuid):
-        return self.session.call_xenapi('VDI.get_by_uuid', vdi_uuid)
+        return self.call_xenapi('VDI.get_by_uuid', vdi_uuid)
 
     def get_host_by_uuid(self, host_uuid):
-        return self.session.call_xenapi('host.get_by_uuid', host_uuid)
+        return self.call_xenapi('host.get_by_uuid', host_uuid)
 
     def create_sr(self, host_ref, device_config, name_label, name_description,
                   sr_type, physical_size=None, content_type=None,
                   shared=False, sm_config=None):
-        return self.session.call_xenapi(
+        return self.call_xenapi(
             'SR.create',
             host_ref,
             device_config,
@@ -53,7 +79,7 @@ class XenAPINFSDriver(object):
 
     def create_vdi(self, sr_ref, size, vdi_type,
                    sharable=False, read_only=False, other_config=None):
-        return self.session.call_xenapi('VDI.create',
+        return self.call_xenapi('VDI.create',
             dict(
                 SR=sr_ref,
                 virtual_size=str(size),
@@ -66,7 +92,7 @@ class XenAPINFSDriver(object):
 
     def introduce_sr(self, sr_uuid, name_label, name_description, sr_type,
                      content_type=None, shared=False, sm_config=None):
-        return self.session.call_xenapi(
+        return self.call_xenapi(
             'SR.introduce',
             sr_uuid,
             name_label,
@@ -78,7 +104,7 @@ class XenAPINFSDriver(object):
         )
 
     def create_pbd(self, host_ref, sr_ref, device_config):
-        return self.session.call_xenapi(
+        return self.call_xenapi(
             'PBD.create',
             dict(
                 host=host_ref,
@@ -88,7 +114,7 @@ class XenAPINFSDriver(object):
         )
 
     def plug_pbd(self, pbd_ref):
-        self.session.call_xenapi('PBD.plug', pbd_ref)
+        self.call_xenapi('PBD.plug', pbd_ref)
 
     # Record based operations
     def get_sr_uuid(self, sr_ref):
@@ -99,6 +125,9 @@ class XenAPINFSDriver(object):
 
     def is_nfs_sr(self, sr_ref):
         return self.get_sr_record(sr_ref).get('type') == 'nfs'
+
+    def get_host_uuid(self, host_ref):
+        return self.get_host_record(host_ref)['uuid']
 
     # Compound operations
     def unplug_pbds_and_forget_sr(self, sr_ref):
@@ -164,33 +193,65 @@ class XenAPINFSDriver(object):
 
         return sr_ref
 
-    # High level operations
+
+class ContextManagedSession(XenAPISession):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+
+def connect(url, user, password):
+    import XenAPI
+    session = XenAPI.Session(url)
+    session.login_with_password(user, password)
+    return ContextManagedSession(session, XenAPI.Failure)
+
+
+class SessionFactory(object):
+    def __init__(self, url, user, password):
+        self.url = url
+        self.user = user
+        self.password = password
+
+    def get_session(self):
+        return connect(self.url, self.user, self.password)
+
+
+class XenAPINFSDriver(object):
+    def __init__(self, session_factory):
+        self._session_factory = session_factory
+
     def create_volume(self, host_uuid, server, serverpath, size):
         'Returns connection_data, which could be used to connect to the vol'
-        host_ref = self.get_host_by_uuid(host_uuid)
-        with self.new_sr_on_nfs(host_ref, server, serverpath) as sr_ref:
-            sr_uuid = self.get_sr_uuid(sr_ref)
-            vdi_ref = self.create_new_vdi(sr_ref, size)
-            vdi_uuid = self.get_vdi_uuid(vdi_ref)
+        with self._session_factory.get_session() as session:
+            host_ref = session.get_host_by_uuid(host_uuid)
+            with session.new_sr_on_nfs(host_ref, server, serverpath) as sr_ref:
+                sr_uuid = session.get_sr_uuid(sr_ref)
+                vdi_ref = session.create_new_vdi(sr_ref, size)
+                vdi_uuid = session.get_vdi_uuid(vdi_ref)
 
-        return dict(
-            sr_uuid=sr_uuid,
-            vdi_uuid=vdi_uuid,
-            server=server,
-            serverpath=serverpath)
+            return dict(
+                sr_uuid=sr_uuid,
+                vdi_uuid=vdi_uuid,
+                server=server,
+                serverpath=serverpath)
 
     def connect_volume(self, host_uuid, connection_data):
-        host_ref = self.get_host_by_uuid(host_uuid)
-        sr_ref = self.plug_nfs_sr(
-            host_ref,
-            connection_data['server'],
-            connection_data['serverpath'],
-            connection_data['sr_uuid']
-        )
-        self.scan_sr(sr_ref)
-        return self.get_vdi_by_uuid(connection_data['vdi_uuid'])
+        with self._session_factory.get_session() as session:
+            host_ref = session.get_host_by_uuid(host_uuid)
+            sr_ref = session.plug_nfs_sr(
+                host_ref,
+                connection_data['server'],
+                connection_data['serverpath'],
+                connection_data['sr_uuid']
+            )
+            session.scan_sr(sr_ref)
+            return session.get_vdi_by_uuid(connection_data['vdi_uuid'])
 
     def disconnect_volume(self, vdi_ref):
-        vdi_rec = self.get_vdi_record(vdi_ref)
-        sr_ref = vdi_rec['SR']
-        self.unplug_pbds_and_forget_sr(sr_ref)
+        with self._session_factory.get_session() as session:
+            vdi_rec = session.get_vdi_record(vdi_ref)
+            sr_ref = vdi_rec['SR']
+            session.unplug_pbds_and_forget_sr(sr_ref)
