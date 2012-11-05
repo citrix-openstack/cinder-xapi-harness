86211bd33b5d9e5418d655186d40e9424d8bb979
Mate Lakat <mate.lakat@citrix.com>
2012-11-05 16:09:17 +0000

    Change coverage parameter to cinder.volume.xenapi

--- run_tests.sh
+++ run_tests.sh
@@ -11,4 +11,4 @@ mkdir -p cinder/volume
 touch cinder/__init__.py
 touch cinder/volume/__init__.py
 cp -R cinder-master/cinder/volume/xenapi cinder/volume/
-nosetests -v -x --cover-html --cover-erase --with-coverage --cover-package=xenapi_nfs_driver
+nosetests -v -x --cover-html --cover-erase --with-coverage --cover-package=cinder.volume.xenapi
