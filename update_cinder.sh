1e1e944cdb1fab3820c1a2339d0313d6fa663548
Mate Lakat <mate.lakat@citrix.com>
2012-12-28 00:06:25 +0000

    Align xenapi driver module path with cinder changes

--- update_cinder.sh
+++ update_cinder.sh
@@ -4,8 +4,8 @@
     git pull
 ) || git clone git://github.com/citrix-openstack/cinder.git cinder-master
 rm -rf cinder
-mkdir -p cinder/volume/driver
+mkdir -p cinder/volume/drivers
 touch cinder/__init__.py
 touch cinder/volume/__init__.py
-touch cinder/volume/driver/__init__.py
-cp -R cinder-master/cinder/volume/driver/xenapi cinder/volume/driver/
+touch cinder/volume/drivers/__init__.py
+cp -R cinder-master/cinder/volume/drivers/xenapi cinder/volume/drivers/
