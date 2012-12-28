e127aec2946f43f39235331dc1e2cf0c150aa3f6
Mate Lakat <mate.lakat@citrix.com>
2012-12-28 00:12:16 +0000

    Add new style coverage

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,7 @@ virtualenv --no-site-packages .env
 . .env/bin/activate
 pip install -r harness-requires
 ./update_cinder.sh
-nosetests -v -x --cover-html --cover-erase --with-coverage --cover-package=cinder.volume.drivers.xenapi
+
+coverage erase
+coverage run nosetests
+coverage report --show-missing --include=cinder.volume.drivers.xenap*
