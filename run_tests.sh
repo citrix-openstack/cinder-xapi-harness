9d065e9e54ea9f0dfbe19505b90bf70c286bf977
Mate Lakat <mate.lakat@citrix.com>
2012-12-28 00:15:37 +0000

    coverage requires full path to nosetests

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,5 @@ pip install -r harness-requires
 ./update_cinder.sh
 
 coverage erase
-coverage run nosetests
+coverage run .env/bin/nosetests
 coverage report --show-missing --include=cinder.volume.drivers.xenap*
