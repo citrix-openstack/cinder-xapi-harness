732a934e9a125bf461e4a11ba9f7a4a41af844ea
Mate Lakat <mate.lakat@citrix.com>
2012-12-28 00:18:56 +0000

    Add verbose, and stop on error options to nosetests

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,5 @@ pip install -r harness-requires
 ./update_cinder.sh
 
 coverage erase
-coverage run .env/bin/nosetests
+coverage run .env/bin/nosetests -v -x
 coverage report --show-missing --include=cinder.volume.drivers.xenap*
