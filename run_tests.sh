7d19cdbb44635073094ec12e1a44556675778649
Mate Lakat <mate.lakat@citrix.com>
2012-12-31 15:56:51 +0000

    run_tests.sh accepts any nosetest parameters, and passes them to nosetests

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,5 @@ pip install -r harness-requires
 ./update_cinder.sh
 
 coverage erase
-coverage run .env/bin/nosetests -v -x
+coverage run .env/bin/nosetests -v -x "$@"
 coverage report --show-missing --include=*xenapi*
