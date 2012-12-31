bce233d70f766366ad10cb2a87638324e905f006
Mate Lakat <mate.lakat@citrix.com>
2012-12-31 15:57:35 +0000

    Use ssh url for cinder check-out

--- update_cinder.sh
+++ update_cinder.sh
@@ -2,7 +2,7 @@
 [ -e cinder-master ] && (
     cd cinder-master
     git pull
-) || git clone git://github.com/citrix-openstack/cinder.git cinder-master
+) || git clone git@github.com:citrix-openstack/cinder.git cinder-master
 rm -rf cinder
 mkdir -p cinder/volume/drivers
 touch cinder/__init__.py
