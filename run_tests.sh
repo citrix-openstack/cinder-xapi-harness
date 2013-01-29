e6d1b8492067c43eaecaf90afc2dad2e339b0aa7
Mate Lakat <mate.lakat@citrix.com>
2013-01-29 15:15:53 +0000

    inline helper scripts to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,69 @@
 #!/bin/bash
-virtualenv --no-site-packages .env
-. .env/bin/activate
-pip install -r harness-requires
-./update_cinder.sh
 
-set -eux
+function set_parameters_from_environment
+{
+cat params.py.template |
+sed -e "s/XAPIPASS/$XAPIPASS/" \
+-e "s/XAPISERVER/$XAPISERVER/" \
+-e "s/NFSSERVER/$NFSSERVER/" \
+-e "s,NFSPATH,$NFSPATH," \
+-e "s,LOCALPATHTONFS,$LOCALPATHTONFS," > params.py
+}
+
+function setup_virtualenv
+{
+(
+virtualenv --no-site-packages "$1"
+set +u
+. "$1/bin/activate"
+set -u
+pip install -r harness-requires
+)
+}
+
+function clone_cinder_repo_to
+{
+[ -e "$1" ] || git clone "$REPO" "$1"
+
+(
+cd "$1"
+git checkout "${BRANCH-master}"
+git pull
+)
+
+}
+
+function extract_xapi_code_from
+{
+rm -rf cinder
+mkdir -p cinder/volume/drivers
+touch cinder/__init__.py
+touch cinder/volume/__init__.py
+touch cinder/volume/drivers/__init__.py
+cp -R $1/cinder/volume/drivers/xenapi cinder/volume/drivers/
+}
+
+function run_tests
+{
+(
+set +u
+. "$1/bin/activate"
+set -u
 coverage erase
+shift
 coverage run .env/bin/nosetests -v -x "$@"
 coverage report --show-missing --include=*xenapi*
+)
+}
+
+set -eux
+
+set_parameters_from_environment
+setup_virtualenv ".env"
+
+BRANCH="${BRANCH-master}"
+REPO="${REPO-git://github.com/citrix-openstack/cinder.git}"
+
+clone_cinder_repo_to "cinder-master"
+extract_xapi_code_from "cinder-master"
+run_tests ".env" "$@"
--- set_parameters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exu
-cat params.py.template |
-sed -e "s/XAPIPASS/$XAPIPASS/" \
--e "s/XAPISERVER/$XAPISERVER/" \
--e "s/NFSSERVER/$NFSSERVER/" \
--e "s,NFSPATH,$NFSPATH," \
--e "s,LOCALPATHTONFS,$LOCALPATHTONFS," > params.py
